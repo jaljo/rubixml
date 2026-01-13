ca29ae485354a3e950fef8386af492e2dc700214
Joris Langlois <joris.langlois@gmail.com>
2026-01-13 16:37:18 +0100

    cifar nn 0.7 accuracy

--- src/cifar-nn.php
+++ src/cifar-nn.php
@@ -5,48 +5,40 @@ require __DIR__ . '/../vendor/autoload.php';
 use Rubix\ML\Classifiers\MultilayerPerceptron;
 use Rubix\ML\CrossValidation\Metrics\Accuracy;
 use Rubix\ML\Datasets\Labeled;
-use Rubix\ML\Extractors\CSV;
 use Rubix\ML\Loggers\Screen;
 use Rubix\ML\NeuralNet\ActivationFunctions\Sigmoid;
 use Rubix\ML\NeuralNet\Layers\Activation;
 use Rubix\ML\NeuralNet\Layers\Dense;
-use Rubix\ML\Transformers\L1Normalizer;
-// use Rubix\ML\Transformers\ImageVectorizer;
-use Rubix\ML\Transformers\NumericStringConverter;
+use Rubix\ML\Transformers\ImageVectorizer;
+use Rubix\ML\Transformers\MinMaxNormalizer;
 
-srand(42);
+srand(0);
 
-// $samples = [];
-// $labels = [];
-// $LIM_SAMPLES = 100;
+$samples = [];
+$labels = [];
+$LIM_SAMPLES = 200;
 
-// foreach (['square', 'circle'] as $class) {
-//   foreach (glob(__DIR__ . '/../data/2D_Geometric_Shapes_Dataset/' . $class . '/*.png') as $idx => $file) {
-//       if ($idx === $LIM_SAMPLES) {
-//         break;
-//       }
+foreach (['cat', 'airplane'] as $class) {
+  foreach (glob(__DIR__ . '/../data/CIFAR-10-images/train/' . $class . '/*.jpg') as $idx => $file) {
+      if ($idx === $LIM_SAMPLES) {
+        break;
+      }
 
-//       $samples[] = [imagecreatefrompng($file)];
-//       $labels[] = $class;
-//   }
-// }
+      $samples[] = [imagecreatefromjpeg($file)];
+      $labels[] = $class;
+  }
+}
 
-// $dataset = Labeled::build($samples, $labels)->apply(new ImageVectorizer())->randomize();
-// [ $train, $val ] = $dataset->stratifiedSplit(0.8);
-
-$iterator = new CSV(__DIR__ . '/../data/iris/iris.data', header: false);
-$dataset = Labeled::fromIterator($iterator)
-    ->apply(new NumericStringConverter())
-    ->apply(new L1Normalizer())
-    ->dropFeature(0)
-    ->dropFeature(0)
-    ->randomize();
-
+$dataset = Labeled::build($samples, $labels)
+  ->apply(new ImageVectorizer())
+  ->apply(new MinMaxNormalizer())
+  ->randomize();
 [ $train, $val ] = $dataset->stratifiedSplit(0.8);
 
 $estimator = new MultilayerPerceptron(
   hiddenLayers: [
-    new Dense(100),
+    new Dense(200),
+    // new Activation(new Sigmoid()),
   ],
 );
 $estimator->setLogger(new Screen());
